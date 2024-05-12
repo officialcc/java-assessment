c3c513dea545281485445350cd67f16e9cb4ccb0
colinchin1977 <xnnproject@yahoo.co.uk>
2024-05-13 01:19:46 +0800

    Updated CourseServiceTest

--- src/test/CourseServiceTest.java
+++ src/test/CourseServiceTest.java
@@ -5,44 +5,34 @@ import com.generation.model.Module;
 import com.generation.service.CourseService;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-@Nested
 class CourseServiceTest {
 
-    CourseService courseService = new CourseService();
+    CourseService courseService;
 
     @BeforeEach
-    public void init() {
-        // Dummy module and courses
-        Module module = new Module("TEST-MODULE", "Test Module", "Test description");
-        courseService.registerCourse(new Course("TEST-COURSE-1", "Test Course 1", 6, module));
-        courseService.registerCourse(new Course("TEST-COURSE-2", "Test Course 2", 6, module));
+    void setUp() {
+        courseService = new CourseService();
     }
 
     @Test
     void registerCourse() {
-        // Assuming you have added some courses in the setup
-        // Register a new course
-        Module newModule = new Module("NEW-MODULE", "New Module", "New description");
-        Course newCourse = new Course("NEW-COURSE", "New Course", 8, newModule);
-        courseService.registerCourse(newCourse);
+        Course course = new Course("TEST-COURSE", "Test Course", 8, new Module("TEST-MODULE", "Test Module", "Test Module Description") );
+
+        courseService.registerCourse(course);
 
         // Test that course was registered successfully
-        Course retrievedCourse = courseService.getCourse("NEW-COURSE");
-        assertNotNull(retrievedCourse);
-        assertEquals("New Course", retrievedCourse.getName());
-        assertEquals(8, retrievedCourse.getCredits());
-        assertEquals("New Module", retrievedCourse.getModule().getName());
+        assertNotNull(courseService.getCourse("TEST-COURSE"));
+        assertEquals(course, courseService.getCourse("TEST-COURSE"));
     }
 
     @Test
     void getCourse() {
         // Test that course code is recognised as correct
-        assertNotNull(courseService.getCourse( "INTRO-CS-1" ));
+        assertNotNull(courseService.getCourse( "INTRO-CS-1"));
         // Test that course code is recognised as incorrect
-        assertNull(courseService.getCourse( "CS-1"));
+        assertNull(courseService.getCourse( "INTRO-CS"));
     }
 }
